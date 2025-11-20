f19ab0406907870342b9544fbe5bc0e2c8aab95c
Hitmandark07 <shekharkumar235838@gmail.com>
2025-11-20 16:51:21 +0530

    prisma added

---
+++ lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+process.on("beforeExit", (): void => {
+  prisma.$disconnect();
+});
+
+export default prisma;
